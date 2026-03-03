68c1e171b95fc86f9a89544a4af8240a1f5237ac
Urielthebigboss <dakaudjury@gmail.com>
2026-03-03 07:10:43 +0100

    chore: add typescript devDependenmmp

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import path from "path"
+
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/',  // ou '/nom-repo/' si repo GitHub sans domaine custom
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, ""),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: '/DrMorieProject/', // Remplace par le nom exact de ton dépôt GitHub
 })
