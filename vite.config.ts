fd3f043d7d5d303c20bab279a8d6bf0cbbfa2c9e
Urielthebigboss <dakaudjury@gmail.com>
2026-03-03 07:45:19 +0100

    chore: add typescript devDependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/DrMorieProject/', // Remplace par le nom exact de ton dépôt GitHub
-})
+  base: '/',
+  plugins: [ react()],
+  
+});
